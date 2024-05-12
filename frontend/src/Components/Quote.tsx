cb2f5453d0e805667b7cbfa8fbeb0873f40705b8
Jain K <kuriakosejain456@gmail.com>
2024-05-12 20:04:11 +0530

    style componenets

---
+++ frontend/src/Components/Quote.tsx
@@ -0,0 +1,17 @@
+const Quote = () => {
+  return (
+    <div className="h-screen bg-slate-200 flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg">
+          <div className=" text-3xl font-bold">
+            "unlock your potenial with our powerful platform"
+          </div>
+          <div className="max-w-md text-xl font-semibold mt-5">
+          "Connect, Collect, Conquer: Empower Your IoT Journey with Us"          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Quote;
